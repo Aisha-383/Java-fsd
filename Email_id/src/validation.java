228ba07eb32fc2fd543b4c15f719a68a68801ad8
Shaik Aisha <118292799+Aisha-383@users.noreply.github.com>
2023-12-26 17:41:19 +0530

    Add files via upload

---
+++ Email_id/src/validation.java
@@ -0,0 +1,40 @@
+	import java.util.ArrayList;
+		import java.util.List;
+		import java.util.Scanner;
+
+public class validation {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	
+
+		        // List of email IDs
+		        List<String> emailList = new ArrayList<>();
+		        emailList.add("aisha@example.com");
+		        emailList.add("shaik@example.com");
+		        emailList.add("mphasis@example.com");
+		        // Add more email IDs as needed
+
+		        // Input from the user
+		        Scanner scanner = new Scanner(System.in);
+		        System.out.print("Enter the email ID to search: ");
+		        String searchEmail = scanner.nextLine();
+
+		        // Searching the email ID
+		        boolean found = searchEmail(emailList, searchEmail);
+
+		        // Displaying result
+		        if (found) {
+		            System.out.println("Email ID found!");
+		        } else {
+		            System.out.println("Email ID not found!");
+		        }
+		    }
+
+		    // Method to search the email ID in the list
+		    public static boolean searchEmail(List<String> emails, String searchEmail) {
+		        return emails.stream().anyMatch(email -> email.equals(searchEmail));
+		    }
+		}
+
+	
