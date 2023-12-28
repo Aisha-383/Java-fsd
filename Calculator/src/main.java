1f7c75d0445eb854523b5945ad2a5fd7c4dd8e99
Aisha-383 <aishashaik383@gmail.com>
2023-12-28 14:44:18 +0530

    uncommit changes

---
+++ Calculator/src/main.java
@@ -0,0 +1,23 @@
+
+import java.util.Scanner;
+
+public class main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        int num1 = scanner.nextInt();
+
+        System.out.print("Enter second number: ");
+        int num2 = scanner.nextInt();
+
+        System.out.print("Enter operator (+, -, *, /): ");
+        char operator = scanner.next().charAt(0);
+
+        calculator.performOperation(operator, num1, num2);
+
+        scanner.close();
+    }
+}
+
+
